dff019acb2434eaf6dbdba911a3527a0e44e18ce
Aparna Shaju <aparnakanatil2002@gmail.com>
2025-12-27 07:46:06 +0530

    socket hook changed

--- src/hooks/socketHook.ts
+++ src/hooks/socketHook.ts
@@ -1,4 +1,11 @@
 import { io } from 'socket.io-client'
 const BASEURL = import.meta.env.VITE_API_URL
 
-export default io(BASEURL, { withCredentials: true, autoConnect: true,transports: ['websocket', 'polling'], })
+export default io(BASEURL, { 
+  withCredentials: true, 
+  autoConnect: true,
+  transports: ['polling', 'websocket'], 
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+})
